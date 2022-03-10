a907f81274a78725be73536f38b121b30dd27d5c
Jan Slabon <jan.slabon@setasign.com>
2022-03-10 15:58:29 +0100

    Update settings.dist.php
    Added apiUrl and ts* entries.

--- examples/settings.dist.php
+++ examples/settings.dist.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
+    'apiUrl' => 'https://t2gtest.globaltrust.eu/trust2go', // 'https://t2g.globaltrust.eu/trust2go',
     'username' => 'your-username',
     'activationPin' => 'your-activation-pin',
     'certificateSerialNumber' => 'your-certificate-serial-number',
+
+    // timestamp configuration
+    'tsUrl' => null, // e.g. 'https://timestamp.globaltrust.eu:13080',
+    'tsUsername' => null, // 'your-timestamp-service-username',
+    'tsPassword' => null, // 'your-timestamp-service-password'
 ];
