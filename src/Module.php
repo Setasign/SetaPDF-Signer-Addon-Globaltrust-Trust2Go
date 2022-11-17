c9884edab4d9684cc9393fe544db53ce6d726a24
Jan Slabon <jan.slabon@setasign.com>
2022-11-17 11:55:19 +0100

    Update Module.php
    Refactored class in view to PHP 8 features.
    Refactored class to use the SetaPDF_Signer_Signature_Module_PadesProxyTrait trait instead of implementing the proxy methods manually.

--- src/Module.php
+++ src/Module.php
@@ -23,25 +23,11 @@ class Module implements
     \SetaPDF_Signer_Signature_DictionaryInterface,
     \SetaPDF_Signer_Signature_DocumentInterface
 {
-    /**
-     * @var Client
-     */
-    protected $client;
+    use \SetaPDF_Signer_Signature_Module_PadesProxyTrait;
 
-    /**
-     * @var string
-     */
-    protected $requestId;
-
-    /**
-     * @var SetaPDF_Signer_Signature_Module_Pades
-     */
-    protected $padesModule;
-
-    /**
-     * @var null|string
-     */
-    protected $certificateSerialNumber;
+    protected Client $client;
+    protected string $requestId;
+    protected string $certificateSerialNumber;
 
     /**
      * @param Client $client
@@ -53,10 +39,9 @@ class Module implements
         $this->client = $client;
         $this->requestId = $requestId;
         $this->certificateSerialNumber = $certificateSerialNumber;
-        $this->padesModule = new SetaPDF_Signer_Signature_Module_Pades();
     }
 
-    public function setDigest(string $digest)
+    public function setDigest(string $digest): void
     {
         if (!\in_array(
             $digest,
@@ -69,61 +54,7 @@ class Module implements
         )) {
             throw new InvalidArgumentException('Invalid digest.');
         }
-        $this->padesModule->setDigest($digest);
-    }
-
-    /**
-     * Set the signing certificate.
-     *
-     * @param string|SetaPDF_Signer_X509_Certificate $certificate PEM encoded certificate, path to the PEM encoded
-     *                                                            certificate or a certificate instance.
-     * @throws InvalidArgumentException
-     * @throws SetaPDF_Signer_Asn1_Exception
-     */
-    public function setCertificate($certificate)
-    {
-        $this->padesModule->setCertificate($certificate);
-    }
-
-    /**
-     * @return SetaPDF_Signer_X509_Certificate|string
-     */
-    public function getCertificate()
-    {
-        return $this->padesModule->getCertificate();
-    }
-
-    /**
-     * Add additional certificates which are placed into the CMS structure.
-     *
-     * @param array|SetaPDF_Signer_X509_Collection $extraCertificates PEM encoded certificates or pathes to PEM encoded
-     *                                                                certificates.
-     * @throws SetaPDF_Signer_Asn1_Exception
-     */
-    public function setExtraCertificates($extraCertificates): void
-    {
-        $this->padesModule->setExtraCertificates($extraCertificates);
-    }
-
-    /**
-     * Adds an OCSP response which will be embedded in the CMS structure.
-     *
-     * @param string|\SetaPDF_Signer_Ocsp_Response $ocspResponse DER encoded OCSP response or OCSP response instance.
-     * @throws SetaPDF_Signer_Exception
-     */
-    public function addOcspResponse($ocspResponse)
-    {
-        $this->padesModule->addOcspResponse($ocspResponse);
-    }
-
-    /**
-     * Adds an CRL which will be embedded in the CMS structure.
-     *
-     * @param string|\SetaPDF_Signer_X509_Crl $crl
-     */
-    public function addCrl($crl)
-    {
-        $this->padesModule->addCrl($crl);
+        $this->_getPadesModule()->setDigest($digest);
     }
 
     /**
@@ -142,40 +73,14 @@ class Module implements
             $this->setExtraCertificates($certificate['chain']);
         }
 
-        $digest = $this->padesModule->getDigest();
+        $padesModule = $this->_getPadesModule();
+        $digest = $padesModule->getDigest();
         // get the hash data from the module
-        $hash = \base64_encode(\hash($digest, $this->padesModule->getDataToSign($tmpPath), true));
-        $this->padesModule->setSignatureValue(
+        $hash = \base64_encode(\hash($digest, $padesModule->getDataToSign($tmpPath), true));
+        $padesModule->setSignatureValue(
             $this->client->sign($this->certificateSerialNumber, $this->requestId, $digest, $hash)
         );
 
-        return (string) $this->padesModule->getCms();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function updateSignatureDictionary(SetaPDF_Core_Type_Dictionary $dictionary): void
-    {
-        $this->padesModule->updateSignatureDictionary($dictionary);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function updateDocument(SetaPDF_Core_Document $document): void
-    {
-        $this->padesModule->updateDocument($document);
-    }
-
-    /**
-     * Get the complete Cryptographic Message Syntax structure.
-     *
-     * @return SetaPDF_Signer_Asn1_Element
-     * @throws SetaPDF_Signer_Exception
-     */
-    public function getCms()
-    {
-        return $this->padesModule->getCms();
+        return (string) $padesModule->getCms();
     }
 }
